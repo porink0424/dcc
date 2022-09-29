912f18745c68b58b33e1827a32b92dcd76328f84
Daichi Kato <daichi5967@docomo.ne.jp>
2022-09-29 18:58:43 +0900

    新たな環境でのtest.shを作成

--- test.sh
+++ test.sh
@@ -10,7 +10,7 @@ echo -e "${PURPLE}test started.${NC}"
 echo
 
 # sample.oを生成
-docker run --rm -v $(cd $(dirname $0) && pwd):/dcc -w /dcc dcc /bin/sh -c "cc -c sample.c"
+riscv64-unknown-elf-gcc -c sample.c
 
 assert() {
     expected="$1"
@@ -21,9 +21,16 @@ assert() {
     if [ $? = 0 ]; then
         # コンパイル成功した場合
 
-        # アセンブル、sample.oとのリンク、実行をdocker環境でやらせる
-        docker run --rm -v $(cd $(dirname $0) && pwd):/dcc -w /dcc dcc /bin/sh -c "cc -c tmp.s; cc -o tmp tmp.o sample.o; ./tmp"
+        # アセンブル
+        riscv64-unknown-elf-gcc -c tmp.s
 
+        # sample.oとのリンク
+        riscv64-unknown-elf-gcc -o tmp tmp.o sample.o
+
+        # 実行
+        spike pk tmp
+
+        # 実行結果の代入
         actual="$?"
 
         echo -e "${YELLOW}\`\`\`$input\`\`\`${NC}"
@@ -44,170 +51,6 @@ assert() {
     fi
 }
 
-assert 13 '
-int main() {
-    int ZoanfA_5ad;
-    int fh978a__4A; 
-    ZoanfA_5ad = (4 * 4) / 2;
-    fh978a__4A = 2 - 3;
-    return (ZoanfA_5ad + fh978a__4A) * 2 - 1;
-}
-'
-assert 1 '
-int main() {
-    int year;
-    int year_quarter;
-
-    year = 2024;
-
-    year_quarter = year / 4;
-    if (year == year_quarter * 4)
-        1;
-    else
-        0;
-}
-'
-assert 22 '
-int main() {
-    int sum;
-    sum = 0;
-    while (sum <= 20)
-        sum = sum + 2;
-    sum;
-}
-'
-assert 55 '
-int main() {
-    int sum;
-    int i;
-    sum = 0;
-    i = 0;
-    for (; i <= 10; i = i + 1)
-        sum = sum + i;
-    return sum;
-}
-'
-# numberが素数なら1を出力するプログラム
-assert 1 '
-int main() {
-    int number;
-    number = 13;
-
-    int flag;
-    flag = 0;
-
-    int i;
-
-    for (i = 2; i < number; i = i + 1) {
-        print(i);
-        if ((number / i) * i == number) {
-            flag = 1;
-        }
-    }
-
-    if (flag) {
-        return 0;
-    } else {
-        return 1;
-    }
-}
-'
-assert 66 '
-int add(int x, int y) {
-    return x + y;
-}
-
-int main() {
-    int i;
-    int sum;
-
-    sum = 0;
-    for (i = 0; i <= 10; i = i + 2) {
-        sum = sum + add(i, i + 1);
-    }
-    return sum;
-}
-'
-assert 55 '
-int fib(int n) {
-    print(n);
-    if (n == 1) {
-        return 1;
-    } else if (n == 2) {
-        return 1;
-    } else {
-        return fib(n-1) + fib(n-2);
-    }
-}
-
-int main() {
-    return fib(10);
-}
-'
-assert 3 '
-int main() {
-    int x;
-    int *y;
-    x = 4;
-    y = &x;
-    *y = 3;
-    return x;
-}
-'
-assert 4 '
-int assign(int *x) {
-    *x = 4;
-}
-
-int main() {
-    int x;
-    int *y;
-    x = 3;
-    y = &x;
-    assign(y);
-    return x;
-}
-'
-assert 12 '
-int main () {
-    int x;
-    int y;
-    x = 82;
-    y = 12;
-    int *z;
-    z = &x;
-    z = z +1;
-    print(*z);
-    return *z;
-}
-'
-assert 4 '
-int main() {
-    int *p;
-    alloc4(&p, 1, 2, 4, 8);
-    int *q;
-    q = p + 2;
-    return *q;
-}
-'
-assert 0 '
-int main() {
-    int x;
-    int *y;
-
-    print(sizeof(x));
-    print(sizeof(y));
-
-    print(sizeof(x+3));
-    print(sizeof(y+3));
-    print(sizeof(*y));
-
-    print(sizeof(1));
-
-    print(sizeof(sizeof(1)));
-
-    return 0;
-}
-'
+assert 0 'aa'
 
 echo -e "${GREEN}test finished successfully.${NC}"
